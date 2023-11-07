97ea2724a615033a6a7cbd705138ebab6ead61cc
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-08 08:11:47 +1100

    update remap

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,3 +1,12 @@
+-- Set leader key to spacebar
 vim.g.mapleader = " "
+
+-- Open file explorer
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+-- Better escape key
 vim.keymap.set("i", "jk", "<Esc>")
+
+-- Navigation remaps to center screen
+vim.keymap.set("i", "<C-u>", "<C-u>zz")
+vim.keymap.set("i", "<C-d>", "<C-d>zz")
