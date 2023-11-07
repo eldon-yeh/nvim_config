d8fb6a0ab911779aa3649219ae35ca05c87f31fb
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-07 22:27:32 +1100

    add harpoon

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -7,6 +7,9 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
+	-- Plenary
+	use('nvim-lua/plenary.nvim')
+
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		-- or                            , branch = '0.1.x',
@@ -20,4 +23,7 @@ return require('packer').startup(function(use)
 
 	-- Treesitter
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	-- Harpoon for file navigation
+	use('ThePrimeagen/harpoon')
 end)
