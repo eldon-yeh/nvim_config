1525a174529c3ed4902a3f7acc2b843334c31cdc
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-08 13:02:37 +1100

    replace null-ls with conform

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -4,54 +4,62 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-	-- Plenary
-	use("nvim-lua/plenary.nvim")
+    -- Plenary
+    use("nvim-lua/plenary.nvim")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.4",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.4",
+        -- or                            , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 
-	-- Colour theme
-	use({ "rose-pine/neovim", as = "rose-pine" })
-	require("rose-pine").setup({ variant = "moon" })
-	vim.cmd("colorscheme rose-pine")
+    -- Colour theme
+    use({ "rose-pine/neovim", as = "rose-pine" })
+    require("rose-pine").setup({ variant = "moon" })
+    vim.cmd("colorscheme rose-pine")
 
-	-- Treesitter
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+    -- Treesitter
+    use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-	-- Harpoon for file navigation
-	use("ThePrimeagen/harpoon")
+    -- Harpoon for file navigation
+    use("ThePrimeagen/harpoon")
 
-	-- Harpoon for file navigation
-	use("mbbill/undotree")
+    -- Harpoon for file navigation
+    use("mbbill/undotree")
 
-	-- Lazygit
-	use({ "kdheepak/lazygit.nvim", requires = { "nvim-lua/plenary.nvim" } })
+    -- Lazygit
+    use({ "kdheepak/lazygit.nvim", requires = { "nvim-lua/plenary.nvim" } })
 
-	-- Lsp
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		requires = {
-			--- Uncomment these if you want to manage LSP servers from neovim
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+    -- Lsp
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-		},
-	})
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
+        },
+    })
 
-	-- Formatter
-	use({ "jose-elias-alvarez/null-ls.nvim", as = "null-ls" })
+    -- Formatter
+    -- use({ "jose-elias-alvarez/null-ls.nvim", as = "null-ls" })
+
+    use({
+        "stevearc/conform.nvim",
+        config = function()
+            require("conform").setup()
+        end,
+    as="conform"})
+
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -84,6 +84,12 @@ _G.packer_plugins = {
     path = "/home/ubuntu/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
+  conform = {
+    config = { "\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fconform\frequire\0" },
+    loaded = true,
+    path = "/home/ubuntu/.local/share/nvim/site/pack/packer/start/conform",
+    url = "https://github.com/stevearc/conform.nvim"
+  },
   harpoon = {
     loaded = true,
     path = "/home/ubuntu/.local/share/nvim/site/pack/packer/start/harpoon",
@@ -108,11 +114,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/ubuntu/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
-  },
-  ["null-ls"] = {
-    loaded = true,
-    path = "/home/ubuntu/.local/share/nvim/site/pack/packer/start/null-ls",
-    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
@@ -157,6 +158,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: conform
+time([[Config for conform]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fconform\frequire\0", "config", "conform")
+time([[Config for conform]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
