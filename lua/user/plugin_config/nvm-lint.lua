c2a87febdaf109587fe7697c9565f52ad52697a2
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-09 19:21:54 +1100

    update linter config

--- lua/user/plugin_config/nvm-lint.lua
+++ lua/user/plugin_config/nvm-lint.lua
@@ -1,6 +1,6 @@
 require('lint').linters_by_ft = {
-    markdown = { 'vale', },
-    html = { 'djlin' },
+    -- markdown = { 'markdownlint', },
+    html = { 'djlint' },
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
