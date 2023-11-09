815f29a0bcef12c3349a0175c6146f33dcb4c175
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-10 08:17:50 +1100

    update config

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -50,3 +50,15 @@ vim.keymap.set("n", "]b", vim.cmd.bn)
 -- Terminal keymaps
 vim.keymap.set("t", "jk", "<C-Bslash><C-n>")
 vim.keymap.set("t", "<Esc>", "<C-Bslash><C-n>")
+
+-- Highlight on yank
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = '*',
+})
