a5cbe546d1889c88e8309dee80dc9e2bcc520330
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-09 20:00:51 +1100

    update config

--- lua/user/plugin_config/conform.lua
+++ lua/user/plugin_config/conform.lua
@@ -1,16 +1,16 @@
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		-- Conform will run multiple formatters sequentially
-		-- python = { "isort", "black" },
-		-- Use a sub-list to run only the first available formatter
-		javascript = { "prettierd" },
-		typescript = { "prettierd" },
-		markdown = { "prettierd" },
-	},
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 10000,
-		lsp_fallback = true,
-	},
+    formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        -- markdown = { "prettierd" },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 10000,
+        lsp_fallback = true,
+    },
 })
