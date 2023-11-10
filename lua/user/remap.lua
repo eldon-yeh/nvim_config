90428c1f728054e3354c2c68a487bf753223ce45
Eldon Yeh <eldon.yeh@gmail.com>
2023-11-10 21:58:38 +1100

    update config

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -34,6 +34,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- linewrap
+vim.keymap.set("n", "<leader>wl", ":set wrap!<cr>")
+
 -- paste without yanking
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
